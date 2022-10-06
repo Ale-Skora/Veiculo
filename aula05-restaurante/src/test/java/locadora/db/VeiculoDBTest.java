0df547087fd613f4b716eebc4d24654cb1850566
Ale_Skora <ale.skora1@gmail.com>
2022-10-06 19:38:31 -0300

    .

---
+++ aula05-restaurante/src/test/java/locadora/db/VeiculoDBTest.java
@@ -0,0 +1,58 @@
+package locadora.db;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class VeiculoDBTest {
+
+    private VeiculoDB restauranteDB = new VeiculoDB();
+
+    @Test
+    public void testCadastrarRestaurante() {
+        Restaurante restaurante
+                = new Restaurante("1", "Pizzaria", "Rua abc", "pizza", 5);
+        Restaurante retorno = restauranteDB.cadastrarRestaurante(restaurante);
+
+        Assertions.assertEquals("1", retorno.getCodigo());
+        Assertions.assertEquals("Pizzaria", retorno.getNome());
+        Assertions.assertEquals("Rua abc", retorno.getEndereco());
+        Assertions.assertEquals("pizza", retorno.getTipo());
+        Assertions.assertEquals(5, retorno.getAvaliacao());
+    }
+
+    @Test
+    public void testListaRestaurantes() {
+        Restaurante restaurante
+            = new Restaurante("1", "Pizzaria", "Rua abc", "pizza", 5);
+        restauranteDB.cadastrarRestaurante(restaurante);
+
+        List<Restaurante> retorno = restauranteDB.listRestaurantes();
+        Assertions.assertTrue(retorno.size() > 0);
+    }
+
+
+    @Test
+    public void testBuscaResturante() {
+        Restaurante restaurante
+                = new Restaurante("10", "Pizzaria", "Rua abc", "pizza", 5);
+        restauranteDB.cadastrarRestaurante(restaurante);
+
+        Restaurante retorno = restauranteDB.buscaRestaurante("10");
+        Assertions.assertEquals("10", retorno.getCodigo());
+        Assertions.assertEquals("Pizzaria", retorno.getNome());
+        Assertions.assertEquals("Rua abc", retorno.getEndereco());
+        Assertions.assertEquals("pizza", retorno.getTipo());
+        Assertions.assertEquals(5, retorno.getAvaliacao());
+    }
+
+    @Test
+    public void testBuscaResturanteNaoExiste() {
+        Restaurante retorno = restauranteDB.buscaRestaurante("100");
+        Assertions.assertNull(retorno);
+    }
+
+
+
+}
